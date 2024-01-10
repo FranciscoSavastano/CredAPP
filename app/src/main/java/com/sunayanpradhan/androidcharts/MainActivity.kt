314c69b328d6b895f6c98a0e43d58bc7cf8cef85
FranciscoSavastano <fransavastanocp@gmail.com>
2024-01-10 15:50:34 -0300

    Inicio do projeto

---
+++ app/src/main/java/com/sunayanpradhan/androidcharts/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.sunayanpradhan.androidcharts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var goBarChart:Button
+
+    lateinit var goPieChart:Button
+
+    lateinit var goRadarChart:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        goPieChart=findViewById(R.id.go_pie_chart)
+
+
+
+        goBarChart.setOnClickListener {
+
+            startActivity(Intent(this,BarChartActivity::class.java))
+
+        }
+
+        goPieChart.setOnClickListener {
+
+            startActivity(Intent(this,PieChartActivity::class.java))
+
+        }
+
+        goRadarChart.setOnClickListener {
+
+            startActivity(Intent(this,RadarChartActivity::class.java))
+
+        }
+
+
+    }
+}
